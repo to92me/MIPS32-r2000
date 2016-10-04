40bc1ca2276d99d0eb353a0078c41caec8b38137
Tomislav Tumbas <to92me@gmail.com>
2016-10-04 22:25:05 +0200

    Rom reads data from file

--- vhdl/src/Rom32.vhd
+++ vhdl/src/Rom32.vhd
@@ -24,6 +24,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.Definitions_pkg.all;
+use STD.TEXTIO.all; 
+--use IEEE.STD_LOGIC_UNSIGNED.all; 
+--use IEEE.STD_LOGIC_ARITH.all;
 
 entity Rom32 is
 	port(
@@ -33,207 +36,53 @@ entity Rom32 is
 	);
 end entity Rom32;
 
-architecture RTL of Rom32 is
-	type rom_t is array (0 to 2 ** 19 - 1) of std8_st;
-	signal rom_0 : rom_t;
-	--	signal rom_1 : rom_t;
-	--	signal rom_2 : rom_t;
-	--	signal rom_3 : rom_t;
-
+architecture behave of Rom32 is
+	
 	alias rom30_cs is addr(31 downto 19);
 	alias rom30_addr is addr(18 downto 0); -- this is module of 512Mb 
-
-begin
 	
-	data <= rom_0(TO_INTEGER(unsigned(rom30_addr) + 3)) & rom_0(TO_INTEGER(unsigned(rom30_addr) + 2)) & rom_0(TO_INTEGER(unsigned(rom30_addr) + 1)) & rom_0(TO_INTEGER(unsigned(rom30_addr)));
-
-	reset_values : process(rst) is
-		variable i, j : integer;
+begin
+	process is
+		file mem_file 		   : TEXT;
+		variable L             : line;
+		variable ch            : character;
+		variable index, result : integer;
+		variable resultstd 	   : std32_st; 
+		type ramtype is array (0 to 2 ** 19 - 1) of std8_st;
+		variable mem : ramtype;
 	begin
-		if (rising_edge(rst)) then
-			for i in 0 to (2 ** 19 - 2) loop
-				rom_0(i)  <= "00000000"; 
+		-- initialize memory from file
+		for i in 0 to 2 ** 19 - 1 loop           -- set all contents low
+			mem(i) := x"00";
+		end loop;
+		index := 0;
+		FILE_OPEN(mem_file, "/home/tome/Work/college/psds/mips32-utils/book_example/app2.hex", READ_MODE);
+		while not endfile(mem_file) loop
+			readline(mem_file, L);
+			result := 0;
+			for i in 1 to 8 loop
+				read(L, ch);
+				if '0' <= ch and ch <= '9' then
+					result := result * 16 + character'pos(ch) - character'pos('0');
+				elsif 'a' <= ch and ch <= 'f' then
+					result := result * 16 + character'pos(ch) - character'pos('a') + 10;
+				else
+					report "Format error on line" & integer'image(index) severity error;
+				end if;
 			end loop;
---			rom_0(3)  <=  "00100000";
---			rom_0(2)  <=  "01000001";
---			rom_0(1)  <=  "00000000";
---			rom_0(0)  <=  "00001100";
---			
---			rom_0(7)  <=  "00100000";
---			rom_0(6)  <=  "01000111";
---			rom_0(5)  <=  "00000000";
---			rom_0(4)  <=  "00001101";
---			
---			rom_0(11)  <=  "00000000";
---			rom_0(10)  <=  "00100111";
---			rom_0(9)  <=  "00101000";
---			rom_0(8)  <=  "00100000";
---			
---			rom_0(15)  <=  "10101100";
---			rom_0(14)  <=  "00000101";
---			rom_0(13)  <=  "00000000";
---			rom_0(12)  <=  "00000000";
---			
---			rom_0(19)  <=  "10001100";
---			rom_0(18)  <=  "00000110";
---			rom_0(17)  <=  "00000000";
---			rom_0(16)  <=  "00000000";
-			
---			rom_0(3)  <=  "00100000";
---			rom_0(2)  <=  "01000001";
---			rom_0(1)  <=  "00000000";
---			rom_0(0)  <=  "00001100";
---			rom_0(7)  <=  "00100000";
---			rom_0(6)  <=  "01000111";
---			rom_0(5)  <=  "00000000";
---			rom_0(4)  <=  "00001101";
---			rom_0(11)  <=  "00000000";
---			rom_0(10)  <=  "00100111";
---			rom_0(9)  <=  "00101000";
---			rom_0(8)  <=  "00100000";
---			rom_0(15)  <=  "10101100";
---			rom_0(14)  <=  "00000101";
---			rom_0(13)  <=  "00000000";
---			rom_0(12)  <=  "00000000";
---			rom_0(19)  <=  "10001100";
---			rom_0(18)  <=  "00000001";
---			rom_0(17)  <=  "00000000";
---			rom_0(16)  <=  "00000000";
---			rom_0(23)  <=  "00001000";
---			rom_0(22)  <=  "00000000";
---			rom_0(21)  <=  "00000000";
---			rom_0(20)  <=  "00000010";
+			resultstd := std_logic_vector(to_unsigned(result, 32)); 
+			mem(index+3) := resultstd(31 downto 24);  
+			mem(index+2) := resultstd(23 downto 16);
+			mem(index+1) := resultstd(15 downto 8);
+			mem(index) := resultstd(7 downto 0);
+			index      := index + 4;
+		end loop;
+		
+		-- read memory
+		loop
+			data <= mem(TO_INTEGER(unsigned(rom30_addr) + 3)) & mem(TO_INTEGER(unsigned(rom30_addr) + 2)) & mem(TO_INTEGER(unsigned(rom30_addr) + 1)) & mem(TO_INTEGER(unsigned(rom30_addr)));
+			wait on addr;
+		end loop;
+	end process;
+end;
 
-
---rom_0(3)  <=  "00100000";
---rom_0(2)  <=  "01000100";
---rom_0(1)  <=  "00000000";
---rom_0(0)  <=  "00000001";
---rom_0(7)  <=  "00100000";
---rom_0(6)  <=  "01000101";
---rom_0(5)  <=  "00000000";
---rom_0(4)  <=  "00001100";
---rom_0(11)  <=  "00100000";
---rom_0(10)  <=  "01000110";
---rom_0(9)  <=  "00000000";
---rom_0(8)  <=  "00001101";
---rom_0(15)  <=  "00000000";
---rom_0(14)  <=  "10000101";
---rom_0(13)  <=  "00111000";
---rom_0(12)  <=  "00100000";
---rom_0(19)  <=  "00000000";
---rom_0(18)  <=  "11100100";
---rom_0(17)  <=  "00111000";
---rom_0(16)  <=  "00100010";
---rom_0(23)  <=  "00010000";
---rom_0(22)  <=  "11100100";
---rom_0(21)  <=  "11111111";
---rom_0(20)  <=  "11111010";
---rom_0(27)  <=  "00001000";
---rom_0(26)  <=  "00000000";
---rom_0(25)  <=  "00000000";
---rom_0(24)  <=  "00000100";
-
---rom_0(3)  <=  "00100000";
---rom_0(2)  <=  "01000001";
---rom_0(1)  <=  "00000000";
---rom_0(0)  <=  "00001100";
---rom_0(7)  <=  "00100000";
---rom_0(6)  <=  "01000111";
---rom_0(5)  <=  "00000000";
---rom_0(4)  <=  "00001101";
---rom_0(11)  <=  "00000000";
---rom_0(10)  <=  "00100111";
---rom_0(9)  <=  "00101000";
---rom_0(8)  <=  "00100000";
---rom_0(15)  <=  "10101100";
---rom_0(14)  <=  "00000101";
---rom_0(13)  <=  "00000000";
---rom_0(12)  <=  "00000000";
---rom_0(19)  <=  "10001100";
---rom_0(18)  <=  "00000001";
---rom_0(17)  <=  "00000000";
---rom_0(16)  <=  "00000000";
---rom_0(23)  <=  "00001000";
---rom_0(22)  <=  "00000000";
---rom_0(21)  <=  "00000000";
---rom_0(20)  <=  "00000010";
-
-rom_0(3)  <=  "00100000";
-rom_0(2)  <=  "00000010";
-rom_0(1)  <=  "00000000";
-rom_0(0)  <=  "00000101";
-rom_0(7)  <=  "00100000";
-rom_0(6)  <=  "00000011";
-rom_0(5)  <=  "00000000";
-rom_0(4)  <=  "00001100";
-rom_0(11)  <=  "00100000";
-rom_0(10)  <=  "01100111";
-rom_0(9)  <=  "11111111";
-rom_0(8)  <=  "11110111";
-rom_0(15)  <=  "00000000";
-rom_0(14)  <=  "11100010";
-rom_0(13)  <=  "00100000";
-rom_0(12)  <=  "00100101";
-rom_0(19)  <=  "00000000";
-rom_0(18)  <=  "01100100";
-rom_0(17)  <=  "00101000";
-rom_0(16)  <=  "00100100";
-rom_0(23)  <=  "00000000";
-rom_0(22)  <=  "10100100";
-rom_0(21)  <=  "00101000";
-rom_0(20)  <=  "00100000";
-rom_0(27)  <=  "00010000";
-rom_0(26)  <=  "10100111";
-rom_0(25)  <=  "00000000";
-rom_0(24)  <=  "00001010";
-rom_0(31)  <=  "00000000";
-rom_0(30)  <=  "01100100";
-rom_0(29)  <=  "00100000";
-rom_0(28)  <=  "00101010";
-rom_0(35)  <=  "00010000";
-rom_0(34)  <=  "10000000";
-rom_0(33)  <=  "00000000";
-rom_0(32)  <=  "00000001";
-rom_0(39)  <=  "00100000";
-rom_0(38)  <=  "00000101";
-rom_0(37)  <=  "00000000";
-rom_0(36)  <=  "00000000";
-rom_0(43)  <=  "00000000";
-rom_0(42)  <=  "11100010";
-rom_0(41)  <=  "00100000";
-rom_0(40)  <=  "00101010";
-rom_0(47)  <=  "00000000";
-rom_0(46)  <=  "10000101";
-rom_0(45)  <=  "00111000";
-rom_0(44)  <=  "00100000";
-rom_0(51)  <=  "00000000";
-rom_0(50)  <=  "11100010";
-rom_0(49)  <=  "00111000";
-rom_0(48)  <=  "00100010";
-rom_0(55)  <=  "10101100";
-rom_0(54)  <=  "01100111";
-rom_0(53)  <=  "00000000";
-rom_0(52)  <=  "01000100";
-rom_0(59)  <=  "10001100";
-rom_0(58)  <=  "00000010";
-rom_0(57)  <=  "00000000";
-rom_0(56)  <=  "01010000";
-rom_0(63)  <=  "00001000";
-rom_0(62)  <=  "00000000";
-rom_0(61)  <=  "00000000";
-rom_0(60)  <=  "00010001";
-rom_0(67)  <=  "00100000";
-rom_0(66)  <=  "00000010";
-rom_0(65)  <=  "00000000";
-rom_0(64)  <=  "00000001";
-rom_0(71)  <=  "10101100";
-rom_0(70)  <=  "00000010";
-rom_0(69)  <=  "00000000";
-rom_0(68)  <=  "01010100";
-
-			
-		end if;
-	end process reset_values;
-
-end architecture RTL;
